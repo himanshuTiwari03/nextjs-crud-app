dcb4fd8b282576cbaf265cfaaf79e993e6fffa8e
himanshuTiwari03 <work.himanshutw@gmail.com>
2025-07-03 11:18:27 +0530

    added online mongodb conn

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,27 +1,14 @@
 import mongoose from "mongoose";
-
-let isConnected = false; // Track the connection status
-
 export async function connectToDB() {
-  if (isConnected) {
-    return;
-  }
-
-  try {
-    await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://himanshutiwari:fBL6QrMSNG7qXDvl@cluster0.uxqcoxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-      dbName: "crudDB", // ✅ add your actual DB name here
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    isConnected = true;
-    console.log("✅ Connected to MongoDB");
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
+    debugger
+    try {
+         mongoose.connect(process.env.MONGODB_URI);
+        console.log('Connected to MongoDB');
+    } catch (err) {
+    console.error('Database connection failed:', err);
     throw err;
-  }
-}
-
-export default connectToDB;
-
+    }
+    }
+    
+   export default connectToDB;
   
