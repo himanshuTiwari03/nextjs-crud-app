caaf3d0ef851dfff5e3909de1d3607d5b230a6b0
himanshuTiwari03 <work.himanshutw@gmail.com>
2025-07-03 12:58:54 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     return (
         <nav className="flex justify-between items-center list-none bg-slate-800 px-8 py-3">
 
-                <Link className="text-white font-bold" href="/"><li>Topics List</li></Link>
+                <Link className="text-white font-bold" href="/"><li>Blog List</li></Link>
                 <Link className="text-white font-bold" href="/addTopic"><li>Add Topic</li></Link>
 
         </nav>
